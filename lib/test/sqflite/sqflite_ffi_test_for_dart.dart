1df469ab2739bdc51ad002145122572ea5e4cbab
Federico Bruzzone <federico.bruzzone.i@gmail.com>
2022-09-25 22:04:27 +0200

    DB: Add set up of `sqflite` & `sqflite_common_ffi`
    I have done the set up of `sqflite` and `sqflite_common_ffi`.
    Right now the interaction with the db is possibile from any OS.
    I have also written 3 tests to verify correct operation.
    I took the liberty of renaming some directories.
    
    Signed-off-by: Federico Bruzzone <federico.bruzzone.i@gmail.com>

---
+++ lib/test/sqflite/sqflite_ffi_test_for_dart.dart
@@ -0,0 +1,34 @@
+// You can also write regular dart test. Instead of using openDatabase
+// (which is flutter only) you have use the proper SqfliteDatabaseFactory
+// to open a database:
+
+@TestOn('vm')
+library sqflite_common_ffi.test.sqflite_ffi_doc_test;
+
+import 'package:sqflite_common/sqlite_api.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  // Init ffi loader if needed.
+  sqfliteFfiInit();
+  test('Simple test', () async {
+    var factory = databaseFactoryFfi;
+    var db = await factory.openDatabase(inMemoryDatabasePath,
+        options: OpenDatabaseOptions(
+            version: 1,
+            onCreate: (db, version) async {
+              await db.execute(
+                  'CREATE TABLE Test (id INTEGER PRIMARY KEY, value TEXT)');
+            }));
+    // Insert some data
+    await db.insert('Test', {'value': 'my_value'});
+
+    // Check content
+    expect(await db.query('Test'), [
+      {'id': 1, 'value': 'my_value'}
+    ]);
+
+    await db.close();
+  });
+}
