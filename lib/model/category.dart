0bdfb94fddb0b2c625ffca74dc0124c9c65f074e
Federico Bruzzone <federico.bruzzone.i@gmail.com>
2022-10-02 23:59:02 +0200

    Test: Add `category_test` to test `category` model
    Signed-off-by: Federico Bruzzone <federico.bruzzone.i@gmail.com>

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -2,7 +2,7 @@ import 'package:sossoldi/model/base_entity.dart';
 
 const String category = 'category';
 
-class BudgetFields extends BaseEntityFields {
+class CategoryFields extends BaseEntityFields {
   static String name = 'name';
 
   static final List<String?> allFields = [
@@ -13,29 +13,30 @@ class BudgetFields extends BaseEntityFields {
   ];
 }
 
-class Budget extends BaseEntity {
+class Category extends BaseEntity {
   final String name;
 
-  const Budget(
+  const Category(
       {int? id, required this.name, DateTime? createdAt, DateTime? updatedAt})
       : super(id: id, createdAt: createdAt, updatedAt: updatedAt);
 
-  Budget copy({int? id, String? name, num? amountLimit, DateTime? updatedAt}) =>
-      Budget(
+  Category copy(
+          {int? id, String? name, num? amountLimit, DateTime? updatedAt}) =>
+      Category(
           id: id ?? this.id,
           name: name ?? this.name,
           createdAt: createdAt ?? this.createdAt,
           updatedAt: updatedAt ?? this.updatedAt);
 
-  static Budget fromJson(Map<String, Object?> json) => Budget(
+  static Category fromJson(Map<String, Object?> json) => Category(
       id: json[BaseEntityFields.id] as int?,
-      name: json[BudgetFields.name] as String,
+      name: json[CategoryFields.name] as String,
       createdAt: DateTime.parse(json[BaseEntityFields.createdAt] as String),
       updatedAt: DateTime.parse(json[BaseEntityFields.updatedAt] as String));
 
   Map<String, Object?> toJson() => {
         BaseEntityFields.id: id,
-        BudgetFields.name: name,
+        CategoryFields.name: name,
         BaseEntityFields.createdAt: createdAt?.toIso8601String(),
         BaseEntityFields.updatedAt: updatedAt?.toIso8601String(),
       };
