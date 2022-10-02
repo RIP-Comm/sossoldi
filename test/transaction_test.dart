5cb8a2881ab6b5a8424285e56f0b6b2de8f171bf
Federico Bruzzone <federico.bruzzone.i@gmail.com>
2022-10-02 23:47:17 +0200

    Test:Add `transaction_test` to `transaction` model
    Signed-off-by: Federico Bruzzone <federico.bruzzone.i@gmail.com>

--- test/transaction_test.dart
+++ test/transaction_test.dart
@@ -7,8 +7,6 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sossoldi/main.dart';
-
 import 'package:sossoldi/model/transaction.dart';
 import 'package:sossoldi/model/base_entity.dart';
 
@@ -21,14 +19,25 @@ void main() {
         type: Type.income,
         idBankAccount: 0,
         idBudget: "Casa",
-        idCategory: 0);
+        idCategory: 0,
+        createdAt: DateTime.utc(2022),
+        updatedAt: DateTime.utc(2022));
 
     Transaction tCopy = t.copy(id: 10);
+
     assert(tCopy.id == 10);
     assert(tCopy.date == t.date);
+    assert(tCopy.amount == t.amount);
+    assert(tCopy.date == t.date);
+    assert(tCopy.type == t.type);
+    assert(tCopy.idBankAccount == t.idBankAccount);
+    assert(tCopy.idBudget == t.idBudget);
+    assert(tCopy.idBudget == t.idBudget);
+    assert(tCopy.createdAt == t.createdAt);
+    assert(tCopy.updatedAt == t.updatedAt);
   });
 
-  test("Test Json Transaction", () {
+  test("Test fromJson Transaction", () {
     Map<String, Object?> json = {
       BaseEntityFields.id: 0,
       TransactionFields.date: DateTime.utc(2022).toIso8601String(),
@@ -43,6 +52,43 @@ void main() {
     };
 
     Transaction t = Transaction.fromJson(json);
+
     assert(t.id == json[BaseEntityFields.id]);
+    assert(t.date.toUtc().toIso8601String() == json[TransactionFields.date]);
+    assert(t.amount == json[TransactionFields.amount]);
+    assert(t.type.index == json[TransactionFields.type]);
+    assert(t.note == json[TransactionFields.note]);
+    assert(t.idBankAccount == json[TransactionFields.idBankAccount]);
+    assert(t.idBudget == json[TransactionFields.idBudget]);
+    assert(t.idCategory == json[TransactionFields.idCategory]);
+    assert(t.createdAt?.toUtc().toIso8601String() ==
+        json[BaseEntityFields.createdAt]);
+    assert(t.updatedAt?.toUtc().toIso8601String() ==
+        json[BaseEntityFields.updatedAt]);
+  });
+
+  test("Test toJson Transaction", () {
+    Transaction t = Transaction(
+        id: 2,
+        date: DateTime.utc(2022),
+        amount: 100,
+        type: Type.income,
+        idBankAccount: 0,
+        idBudget: "Casa",
+        idCategory: 0);
+
+    Map<String, Object?> json = t.toJson();
+    assert(t.id == json[BaseEntityFields.id]);
+    assert(t.date.toUtc().toIso8601String() == json[TransactionFields.date]);
+    assert(t.amount == json[TransactionFields.amount]);
+    assert(t.type.index == json[TransactionFields.type]);
+    assert(t.note == json[TransactionFields.note]);
+    assert(t.idBankAccount == json[TransactionFields.idBankAccount]);
+    assert(t.idBudget == json[TransactionFields.idBudget]);
+    assert(t.idCategory == json[TransactionFields.idCategory]);
+    assert(t.createdAt?.toUtc().toIso8601String() ==
+        json[BaseEntityFields.createdAt]);
+    assert(t.updatedAt?.toUtc().toIso8601String() ==
+        json[BaseEntityFields.updatedAt]);
   });
 }
