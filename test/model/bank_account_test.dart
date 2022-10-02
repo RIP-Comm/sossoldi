b6d1ddde8afcd80d39b6fee9205c34af6d7f528b
Federico Bruzzone <federico.bruzzone.i@gmail.com>
2022-10-03 00:09:10 +0200

    Test: `bank_account_test` to `bank_account` model
    Signed-off-by: Federico Bruzzone <federico.bruzzone.i@gmail.com>

--- test/model/bank_account_test.dart
+++ test/model/bank_account_test.dart
@@ -1,59 +1,59 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sossoldi/model/budget.dart';
+import 'package:sossoldi/model/bank_account.dart';
 import 'package:sossoldi/model/base_entity.dart';
 
 void main() {
-  test('Test Copy Budget', () {
-    Budget b = Budget(
+  test('Test Copy BankAccount', () {
+    BankAccount b = BankAccount(
         id: 2,
         name: "name",
-        amountLimit: 100,
+        value: 100,
         createdAt: DateTime.utc(2022),
         updatedAt: DateTime.utc(2022));
 
-    Budget bCopy = b.copy(id: 10);
+    BankAccount bCopy = b.copy(id: 10);
 
     assert(bCopy.id == 10);
     assert(bCopy.name == b.name);
-    assert(bCopy.amountLimit == bCopy.amountLimit);
+    assert(bCopy.value == bCopy.value);
     assert(bCopy.createdAt == b.createdAt);
     assert(bCopy.updatedAt == b.updatedAt);
   });
 
-  test("Test fromJson Budget", () {
+  test("Test fromJson BankAccount", () {
     Map<String, Object?> json = {
       BaseEntityFields.id: 0,
-      BudgetFields.name: "Home",
-      BudgetFields.amountLimit: 100,
+      BankAccountFields.name: "Home",
+      BankAccountFields.value: 100,
       BaseEntityFields.createdAt: DateTime.utc(2022).toIso8601String(),
       BaseEntityFields.updatedAt: DateTime.utc(2022).toIso8601String(),
     };
 
-    Budget b = Budget.fromJson(json);
+    BankAccount b = BankAccount.fromJson(json);
 
     assert(b.id == json[BaseEntityFields.id]);
-    assert(b.name == json[BudgetFields.name]);
-    assert(b.amountLimit == json[BudgetFields.amountLimit]);
+    assert(b.name == json[BankAccountFields.name]);
+    assert(b.value == json[BankAccountFields.value]);
     assert(b.createdAt?.toUtc().toIso8601String() ==
         json[BaseEntityFields.createdAt]);
     assert(b.updatedAt?.toUtc().toIso8601String() ==
         json[BaseEntityFields.updatedAt]);
   });
 
-  test("Test toJson Budget", () {
-    Budget b = Budget(
+  test("Test toJson BankAccount", () {
+    BankAccount b = BankAccount(
         id: 2,
         name: "Home",
-        amountLimit: 100,
+        value: 100,
         createdAt: DateTime.utc(2022),
         updatedAt: DateTime.utc(2022));
 
     Map<String, Object?> json = b.toJson();
 
     assert(b.id == json[BaseEntityFields.id]);
-    assert(b.name == json[BudgetFields.name]);
-    assert(b.amountLimit == json[BudgetFields.amountLimit]);
+    assert(b.name == json[BankAccountFields.name]);
+    assert(b.value == json[BankAccountFields.value]);
     assert(b.createdAt?.toUtc().toIso8601String() ==
         json[BaseEntityFields.createdAt]);
     assert(b.updatedAt?.toUtc().toIso8601String() ==
