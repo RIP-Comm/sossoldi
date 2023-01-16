6c747f1d59a34926ad968700dbc56090203d6e46
Narcis Antonio Miclaus <miclaus.na@gmail.com>
2023-01-16 09:45:39 +0100

    Staged changes for GeneratedPluginRegistrant.swift

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -1,14 +1,15 @@
 //
 //  Generated file. Do not edit.
 //
-
 import FlutterMacOS
 import Foundation
 
+import path_provider_macos
 import sqflite
 import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
