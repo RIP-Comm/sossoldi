016d45ead7197742cb4a54cc53c36300f70988c6
theperu <peruginimarco.mp@gmail.com>
2023-03-29 20:50:27 +0200

    Simple implementation

--- lib/custom_widgets/bar_chart/individual_bar.dart
+++
@@ -1,6 +0,0 @@
-class IndividulaBar{
-  final int x;
-  final num y;
-
-  IndividulaBar({required this.x, required this.y});
-}
--- lib/pages/statistics_page.dart
+++ lib/pages/statistics_page.dart
@@ -1,15 +1,13 @@
 // Satistics page.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../constants/functions.dart';
 import '../constants/style.dart';
-import '../custom_widgets/accounts_sum.dart';
 import '../custom_widgets/line_chart.dart';
 import 'package:fl_chart/fl_chart.dart';
-import '../custom_widgets/bar_chart/bar_chart.dart';
 import '../model/bank_account.dart';
 import '../providers/accounts_provider.dart';
+
 
 class StatsPage extends ConsumerStatefulWidget {
   const StatsPage({super.key});
@@ -23,6 +21,7 @@ class _StatsPageState extends ConsumerState<StatsPage> with Functions {
   @override
   Widget build(BuildContext context) {
     final accountList = ref.watch(accountsProvider);
+
     return ListView(
       children: [
         Column(
@@ -106,45 +105,108 @@ class _StatsPageState extends ConsumerState<StatsPage> with Functions {
               minY: -5.0,
               maxDays: 12.0,
             ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                padding: const EdgeInsets.only(left: 8.0, top: 8.0),
+                child: Text(
+                  "Accounts",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6
+                      ?.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.w600,
+                        ),
+                  textAlign: TextAlign.left,
+                ),  
+              ),
+            ),
             Card(
               child: SizedBox(
-                height: 86.0,
                 child: accountList.when(
-                  data: (accounts) => ListView.builder(
-                    itemCount: accounts.length,
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                    physics: const BouncingScrollPhysics(),
-                    scrollDirection: Axis.vertical,
-                    itemBuilder: (context, i) {
-                      if (i == accounts.length) {
-                        return Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 4, 0, 16),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              boxShadow: [defaultShadow],
-                            ),
-                          ),
-                        );
-                      } else {
-                        BankAccount account = accounts[i];
-                        return SizedBox(
-                          height: 20.0,
-                          child: Row(
-                            children: [
-                              Text(account.name),
-                              Expanded(
-                                child: Text(
-                                  account.value.toString(),
-                                  textAlign: TextAlign.right,
-                                ),
+                  data: (accounts) => Flexible(
+                    child: ListView.builder(
+                      itemCount: accounts.length,
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0),
+                      physics: const BouncingScrollPhysics(),
+                      scrollDirection: Axis.vertical,
+                      itemBuilder: (context, i) {
+                        if (i == accounts.length) {
+                          return Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 4, 0, 16),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                boxShadow: [defaultShadow],
                               ),
-                            ],
-                          )
-                        );
-                      }
-                    },
+                            ),
+                          );
+                        } else {
+                          BankAccount account = accounts[i];
+                          return SizedBox(
+                            height: 50.0,
+                            child: Column(
+                              children: [
+                                const Padding(padding: EdgeInsets.all(2.0)),
+                                Row(
+                                  children: [
+                                    RichText(
+                                      textScaleFactor: MediaQuery.of(context).textScaleFactor,
+                                      text: TextSpan(
+                                        children: [
+                                          TextSpan(
+                                            text: account.name,
+                                            style: Theme.of(context).textTheme.bodySmall?.copyWith(color: blue1),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Text(
+                                        "${account.value}€",
+                                        textAlign: TextAlign.right,
+                                        style: Theme.of(context).textTheme.bodySmall?.copyWith(color: blue1),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const Padding(padding: EdgeInsets.only(top: 4.0)),
+                                SizedBox(
+                                  height: 12,
+                                  width: double.infinity,
+                                  child: Row(                                    
+                                    children: [
+                                      Container(
+                                        width: 350*(account.value.toDouble()/3823.56),
+                                        decoration: const BoxDecoration(
+                                          borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(4.0),
+                                            bottomLeft: Radius.circular(4.0),
+                                          ),
+                                          color: blue3,
+                                        ),
+                                      ),
+                                      Container(
+                                        width: 350-350*(account.value.toDouble()/3823.56),
+                                        decoration: const BoxDecoration(
+                                          borderRadius: BorderRadius.only(
+                                            topRight: Radius.circular(4.0),
+                                            bottomRight: Radius.circular(4.0),
+                                          ),
+                                          color: blue7,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            )
+                          );
+                        }
+                      },
+                    ),
                   ),
                   loading: () => const SizedBox(),
                   error: (err, stack) => Text('Error: $err'),
