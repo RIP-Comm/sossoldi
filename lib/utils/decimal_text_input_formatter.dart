0e53c41375f0a2ea801ef3942fa1b9f3d092122e
Roberto Casula <roberto@casula.dev>
2023-07-19 07:53:30 +0200

    Allow decimal input in AddTransaction page. Fixes #95
    Add custom formatter to fix various inconsistencies

---
+++ lib/utils/decimal_text_input_formatter.dart
@@ -0,0 +1,44 @@
+import 'dart:math' as math;
+import 'package:flutter/services.dart';
+
+class DecimalTextInputFormatter extends TextInputFormatter {
+  DecimalTextInputFormatter({this.decimalDigits})
+      : assert(decimalDigits == null || decimalDigits > 0);
+
+  final int? decimalDigits;
+  final String decimalSeparator = ".";
+
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    TextSelection newSelection = newValue.selection;
+    String value = newValue.text;
+
+    if (decimalSeparator.allMatches(value).length > 1) {
+      return oldValue;
+    }
+    
+    if (value == decimalSeparator) {
+      // Allow for .x decimal notation
+      value = "0$decimalSeparator";
+    }
+
+    if (decimalDigits != null && 
+        value.contains(decimalSeparator) && value.substring(value.indexOf(decimalSeparator) + 1).length > decimalDigits!) {
+      value = oldValue.text;
+    }
+
+    newSelection = newValue.selection.copyWith(
+      baseOffset: math.min(value.length, value.length + 1),
+      extentOffset: math.min(value.length, value.length + 1),
+    );
+
+    return TextEditingValue(
+      text: value,
+      selection: newSelection,
+      composing: TextRange.empty,
+    );
+  }
+}
