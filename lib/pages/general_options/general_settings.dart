cf09c10ad4a638c5d02cf071682e5e0827e8a634
Baggi <mattia.banshee@gmail.com>
2023-09-13 23:32:44 +0200

    added static buttons on general settings page

--- lib/pages/general_options/general_settings.dart
+++ lib/pages/general_options/general_settings.dart
@@ -1,16 +1,39 @@
+import 'package:country_flags/country_flags.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
+
+import '../../constants/style.dart';
 
 class GeneralSettingsPage extends ConsumerStatefulWidget {
   const GeneralSettingsPage({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
-  ConsumerState<GeneralSettingsPage> createState() => _GeneralSettingsPageState();
+  ConsumerState<GeneralSettingsPage> createState() =>
+      _GeneralSettingsPageState();
 }
 
-
 class _GeneralSettingsPageState extends ConsumerState<GeneralSettingsPage> {
+  //default values
+  bool darkMode = false;
+  String selectedCurrency = "EUR";
+  String selectedLanguage = "GB";
+
+  List<List<String>> languages = [
+    ["GB", "English"],
+    ["IT", "Italiano"],
+    ["FR", "Français"],
+    ["DE", "Deutsch"]
+  ];
+
+  List<List<String>> currencies = [
+    ["£", "British Pound"],
+    ["€", "Euro"],
+    ["\$", "US Dollar"],
+    ["CHF", "Swiss Franc"],
+    ["¥", "Yuan"],
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,13 +60,163 @@ class _GeneralSettingsPageState extends ConsumerState<GeneralSettingsPage> {
         ),
       ),
       body: SingleChildScrollView(
+        padding: const EdgeInsets.only(left: 20, right: 20, top: 25),
         physics: const BouncingScrollPhysics(),
         child: Column(
           children: [
-
+            Row(
+              children: [
+                Text("Appearance",
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.primary)),
+                const Spacer(),
+                CircleAvatar(
+                    radius: 30.0,
+                    backgroundColor: blue5,
+                    child: IconButton(
+                      color: blue5,
+                      onPressed: () {
+                        setState(() {
+                          darkMode = !darkMode;
+                        });
+                      },
+                      icon: Icon(
+                        darkMode ? Icons.dark_mode : Icons.light_mode,
+                        size: 25.0,
+                        color: Theme.of(context).colorScheme.background,
+                      ),
+                    )),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                Text("Currency",
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.primary)),
+                const Spacer(),
+                GestureDetector(
+                    onTap: () {
+                      sellectCurrency();
+                    },
+                    child: CircleAvatar(
+                        radius: 30.0,
+                        backgroundColor: blue5,
+                        child: Center(
+                            child: Text(
+                          NumberFormat().simpleCurrencySymbol(selectedCurrency),
+                          style: const TextStyle(color: white, fontSize: 25),
+                        )))),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                Text("Language",
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.primary)),
+                const Spacer(),
+                GestureDetector(
+                    onTap: () {
+                      selectLanguage();
+                    },
+                    child: CircleAvatar(
+                      radius: 30.0,
+                      backgroundColor: blue5,
+                      child: Center(
+                          child: CountryFlag.fromCountryCode(
+                        selectedLanguage,
+                        height: 40,
+                        width: 40,
+                        borderRadius: 0,
+                      )),
+                    )),
+              ],
+            ),
           ],
         ),
       ),
     );
+  }
+
+  selectLanguage() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+            title: Text('Select a language',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.primary)),
+            content: SizedBox(
+              height: 220,
+              width: 220,
+              child: ListView.builder(
+                  itemCount: languages.length,
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedLanguage = languages.elementAt(index)[0];
+                          });
+                          Navigator.pop(context);
+                        },
+                        child: ListTile(
+                          leading: CountryFlag.fromCountryCode(
+                            languages.elementAt(index)[0],
+                            height: 40,
+                            width: 40,
+                            borderRadius: 0,
+                          ),
+                          title: Text(
+                            languages.elementAt(index)[1],
+                            textAlign: TextAlign.center,
+                          ),
+                        ));
+                  }),
+            )));
+  }
+
+  sellectCurrency() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+            title: Text('Select a currency',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.primary)),
+            content: SizedBox(
+              height: 220,
+              width: 220,
+              child: ListView.builder(
+                  itemCount: currencies.length,
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedCurrency = currencies.elementAt(index)[0];
+                          });
+                          Navigator.pop(context);
+                        },
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            radius: 22,
+                            backgroundColor: blue5,
+                            child: Text(currencies.elementAt(index)[0],
+                                style: const TextStyle(
+                                    color: Colors.white, fontSize: 20)),
+                          ),
+                          title: Text(
+                            currencies.elementAt(index)[1],
+                            textAlign: TextAlign.center,
+                          ),
+                        ));
+                  }),
+            )));
   }
 }
