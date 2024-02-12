d8244017a3f705357e63b04948cadebfe3ab6ad1
K-w-e <alex.guerra988@gmail.com>
2024-02-12 22:34:36 +0100

    Removed useless import

--- lib/providers/currency_provider.dart
+++ lib/providers/currency_provider.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../constants/constants.dart';
 import '../model/currency.dart';
 
 final currencyStateNotifier = ChangeNotifierProvider(
