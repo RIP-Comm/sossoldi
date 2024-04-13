53e16e1c29af846783175592df97afdd1593a56e
Diego ~ <diomedi79@gmail.com>
2024-04-13 19:17:12 +0200

    Enhanced theme handling: Added support for dark mode, auto  theme based on system settings, and persistent theme state across app  restarts. Also fixed alignment issue with a button.

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -3,23 +3,53 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../constants/constants.dart';
 // import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final appThemeStateNotifier = ChangeNotifierProvider(
-  (ref) => AppThemeState(),
+      (ref) => AppThemeState(),
 );
 
 class AppThemeState extends ChangeNotifier {
-  var isDarkModeEnabled = false;
+  ThemeMode themeMode = ThemeMode.system; // Impostato di default su system
 
-  void setLightTheme() {
-    isDarkModeEnabled = false;
-    updateColorsBasedOnTheme(isDarkModeEnabled);
+  AppThemeState() {
+    loadThemePreference();
+  }
+
+  Future<void> saveThemePreference(String theme) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('themeMode', theme);
+  }
+
+  Future<void> loadThemePreference() async {
+    final prefs = await SharedPreferences.getInstance();
+    final themeModeString = prefs.getString('themeMode') ?? 'system';
+    if (themeModeString == 'light') {
+      themeMode = ThemeMode.light;
+    } else if (themeModeString == 'dark') {
+      themeMode = ThemeMode.dark;
+    } else {
+      themeMode = ThemeMode.system;
+    }
     notifyListeners();
   }
 
-  void setDarkTheme() {
-    isDarkModeEnabled = true;
-    updateColorsBasedOnTheme(isDarkModeEnabled);
+  void setLightTheme() async {
+    themeMode = ThemeMode.light;
     notifyListeners();
+    await saveThemePreference('light');
   }
+
+  void setDarkTheme() async {
+    themeMode = ThemeMode.dark;
+    notifyListeners();
+    await saveThemePreference('dark');
+  }
+
+  void setAutoTheme() async {
+    themeMode = ThemeMode.system;
+    notifyListeners();
+    await saveThemePreference('system');
+  }
+
 }
