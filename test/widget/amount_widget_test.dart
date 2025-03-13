06469d2ef58deeb71c0066c8378919b606ccb235
bongio94 <alezak94@gmail.com>
2025-03-13 12:58:53 +0100

    test: added test cases for comma and decimals number, refactor

--- test/widget/amount_widget_test.dart
+++ test/widget/amount_widget_test.dart
@@ -5,29 +5,40 @@ import 'package:sossoldi/pages/add_page/widgets/amount_widget.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() {
-  databaseFactory = databaseFactoryFfi;
+  // I'm not 100% familiar with how this works but for some reason when adding the last two test cases
+  // it would throw and error after the tests were completed successfully.
+  //
+  // Seems like using the NoIsolate solves the problem.
+  databaseFactory = databaseFactoryFfiNoIsolate;
+
+  const amountWidgetKey = Key('amountWidgetKey');
+  var amountController = TextEditingController();
+
+  setUp(() {
+    amountController = TextEditingController();
+  });
+
+  Widget amountWidget() {
+    return MaterialApp(
+      home: Material(
+        child: ProviderScope(
+          child: AmountWidget(
+            amountController,
+            key: amountWidgetKey,
+          ),
+        ),
+      ),
+    );
+  }
+
   testWidgets(
     'GIVEN an AmountWidget widget '
     'WHEN a period character is inserted '
     'THEN the period character can be found',
     (WidgetTester tester) async {
-      const amountWidgetKey = Key('amountWidgetKey');
       const numberWithOnePeriod = "10.1";
 
-      final amountController = TextEditingController();
-
-      await tester.pumpWidget(
-        MaterialApp(
-          home: Material(
-            child: ProviderScope(
-              child: AmountWidget(
-                amountController,
-                key: amountWidgetKey,
-              ),
-            ),
-          ),
-        ),
-      );
+      await tester.pumpWidget(amountWidget());
 
       final findAmountWidget = find.byKey(amountWidgetKey);
       expect(findAmountWidget, findsOneWidget);
@@ -48,23 +59,9 @@ void main() {
     'WHEN more than one period characters are inserted '
     'THEN no text is found',
     (WidgetTester tester) async {
-      const amountWidgetKey = Key('amountWidgetKey');
       const numberWithTwoPeriods = "10.1.2";
 
-      final amountController = TextEditingController();
-
-      await tester.pumpWidget(
-        MaterialApp(
-          home: Material(
-            child: ProviderScope(
-              child: AmountWidget(
-                amountController,
-                key: amountWidgetKey,
-              ),
-            ),
-          ),
-        ),
-      );
+      await tester.pumpWidget(amountWidget());
 
       final findAmountWidget = find.byKey(amountWidgetKey);
       expect(findAmountWidget, findsOneWidget);
@@ -73,6 +70,57 @@ void main() {
       await tester.pumpAndSettle();
 
       await tester.enterText(findAmountWidget, numberWithTwoPeriods);
+
+      final findPeriod = find.textContaining('.');
+      expect(findPeriod, findsNothing);
+      expect(amountController.text.isEmpty, isTrue);
+    },
+  );
+
+  testWidgets(
+    'GIVEN an AmountWidget widget '
+    'WHEN a comma is used instead of a period '
+    'THEN the comma is replaced with a period',
+    (WidgetTester tester) async {
+      const numberWithComma = "10,1";
+
+      await tester.pumpWidget(amountWidget());
+
+      final findAmountWidget = find.byKey(amountWidgetKey);
+      expect(findAmountWidget, findsOneWidget);
+
+      await tester.tap(findAmountWidget);
+      await tester.pumpAndSettle();
+
+      await tester.enterText(findAmountWidget, numberWithComma);
+
+      final findPeriod = find.textContaining('.');
+      expect(findPeriod, findsOneWidget);
+      expect(
+        amountController.text,
+        equals(
+          numberWithComma.replaceAll(',', '.'),
+        ),
+      );
+    },
+  );
+
+  testWidgets(
+    'GIVEN an AmountWidget widget '
+    'WHEN a number with more than two decimal digits is used '
+    'THEN the number is not allowed',
+    (WidgetTester tester) async {
+      const numberWithThreeDecimals = "10.123";
+
+      await tester.pumpWidget(amountWidget());
+
+      final findAmountWidget = find.byKey(amountWidgetKey);
+      expect(findAmountWidget, findsOneWidget);
+
+      await tester.tap(findAmountWidget);
+      await tester.pumpAndSettle();
+
+      await tester.enterText(findAmountWidget, numberWithThreeDecimals);
 
       final findPeriod = find.textContaining('.');
       expect(findPeriod, findsNothing);
