bf291a24d7b90da4cc5819e46751fde9012833c4
real-Hafez <mahmoodhafez145@gmail.com>
2025-03-20 00:56:29 +0200

    Ensure keyboard appears when adding/editing a budget

--- lib/pages/planning_page/manage_budget_page.dart
+++ lib/pages/planning_page/manage_budget_page.dart
@@ -51,123 +51,131 @@ class _ManageBudgetPageState extends ConsumerState<ManageBudgetPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(15),
-          child: Text(
-            "Select the categories to create your budget",
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'CATEGORY',
-                textAlign: TextAlign.left,
-              ),
-              Text(
-                'AMOUNT',
-                textAlign: TextAlign.right,
-              ),
-            ],
-          ),
-        ),
-        Expanded(
-          child: Column(
-            children: [
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: budgets.length,
-                itemBuilder: (context, index) {
-                  return Dismissible(
-                    key: Key(budgets[index].idCategory.toString()),
-                    background: Container(
-                      padding: const EdgeInsets.only(right: 20.0),
-                      alignment: Alignment.centerRight,
-                      color: Colors.red,
-                      child: const Text(
-                        'Delete',
-                        textAlign: TextAlign.right,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                    onDismissed: (direction) {
-                      deleteBudget(budgets[index], index);
-                    },
-                    child: BudgetCategorySelector(
-                      categories: categories,
-                      categoriesAlreadyUsed: categories
-                          .where((element) => budgets.map((e) => e.name).contains(element.name))
-                          .map((e) => e.name)
-                          .toList(),
-                      budget: budgets[index],
-                      initSelectedCategory: categories
-                              .where((element) => element.id == budgets[index].idCategory)
-                              .isEmpty
-                          ? categories[0]
-                          : categories
-                              .where((element) => element.id == budgets[index].idCategory)
-                              .first,
-                      onBudgetChanged: (updatedBudget) {
-                        updateBudget(updatedBudget, index);
-                      },
-                    ),
-                  );
-                },
-              ),
-              SizedBox(height: 8),
-              Text("Swipe left to delete", style: Theme.of(context).textTheme.bodySmall),
-              SizedBox(height: 12),
-              TextButton.icon(
-                icon: Icon(Icons.add_circle, size: 32),
-                onPressed: () {
-                  setState(() {
-                    budgets.add(Budget(
-                        active: true,
-                        amountLimit: 100,
-                        idCategory: categories[0].id!,
-                        name: categories[0].name));
-                  });
-                },
-                label: Text("Add category budget"),
-              ),
-            ],
-          ),
-        ),
-        const SizedBox(height: 16),
-        Text(
-          "Your monthly budget will be: ${budgets.isEmpty ? 0 : budgets.fold(0, (sum, e) => sum + e.amountLimit.toInt())}€",
-          style: Theme.of(context).textTheme.titleMedium,
-        ),
-        const SizedBox(height: 16),
-        const Divider(indent: 16, endIndent: 16),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () async {
-                for (var item in deletedBudgets) {
-                  await BudgetMethods().deleteByCategory(item.idCategory);
-                }
-                for (var item in budgets) {
-                  await BudgetMethods().insertOrUpdate(item);
-                }
-                setState(() {
-                  widget.onRefreshBudgets();
-                  Navigator.of(context).pop();
-                });
-              },
-              style: ElevatedButton.styleFrom(elevation: 2),
-              child: Text("SAVE BUDGET"),
+    return AnimatedPadding(
+            padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context)
+              .viewInsets
+              .bottom),
+          duration: Duration(milliseconds: 100),
+
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Text(
+              "Select the categories to create your budget",
+              style: Theme.of(context).textTheme.titleLarge,
             ),
           ),
-        )
-      ],
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'CATEGORY',
+                  textAlign: TextAlign.left,
+                ),
+                Text(
+                  'AMOUNT',
+                  textAlign: TextAlign.right,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: budgets.length,
+                  itemBuilder: (context, index) {
+                    return Dismissible(
+                      key: Key(budgets[index].idCategory.toString()),
+                      background: Container(
+                        padding: const EdgeInsets.only(right: 20.0),
+                        alignment: Alignment.centerRight,
+                        color: Colors.red,
+                        child: const Text(
+                          'Delete',
+                          textAlign: TextAlign.right,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      onDismissed: (direction) {
+                        deleteBudget(budgets[index], index);
+                      },
+                      child: BudgetCategorySelector(
+                        categories: categories,
+                        categoriesAlreadyUsed: categories
+                            .where((element) => budgets.map((e) => e.name).contains(element.name))
+                            .map((e) => e.name)
+                            .toList(),
+                        budget: budgets[index],
+                        initSelectedCategory: categories
+                                .where((element) => element.id == budgets[index].idCategory)
+                                .isEmpty
+                            ? categories[0]
+                            : categories
+                                .where((element) => element.id == budgets[index].idCategory)
+                                .first,
+                        onBudgetChanged: (updatedBudget) {
+                          updateBudget(updatedBudget, index);
+                        },
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(height: 8),
+                Text("Swipe left to delete", style: Theme.of(context).textTheme.bodySmall),
+                SizedBox(height: 12),
+                TextButton.icon(
+                  icon: Icon(Icons.add_circle, size: 32),
+                  onPressed: () {
+                    setState(() {
+                      budgets.add(Budget(
+                          active: true,
+                          amountLimit: 100,
+                          idCategory: categories[0].id!,
+                          name: categories[0].name));
+                    });
+                  },
+                  label: Text("Add category budget"),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            "Your monthly budget will be: ${budgets.isEmpty ? 0 : budgets.fold(0, (sum, e) => sum + e.amountLimit.toInt())}€",
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          const SizedBox(height: 16),
+          const Divider(indent: 16, endIndent: 16),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () async {
+                  for (var item in deletedBudgets) {
+                    await BudgetMethods().deleteByCategory(item.idCategory);
+                  }
+                  for (var item in budgets) {
+                    await BudgetMethods().insertOrUpdate(item);
+                  }
+                  setState(() {
+                    widget.onRefreshBudgets();
+                    Navigator.of(context).pop();
+                  });
+                },
+                style: ElevatedButton.styleFrom(elevation: 2),
+                child: Text("SAVE BUDGET"),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
