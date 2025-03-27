d0b68b93f258a62095197b86ea7040f13b84ae25
Simone Antonacci <simo.antonacci@gmail.com>
2025-03-27 19:40:39 +0000

    ets

--- test/e2e/config/setup_config.py
+++ test/e2e/config/setup_config.py
@@ -1,12 +1,13 @@
 import subprocess
 
-emulator_name = "emulator-5554"
+emulator_name = "emulator-5555"
 file_path = "platform_config_template.yaml"
 
 command = f"adb -s {emulator_name} shell getprop emu.uuid"
 
 ret = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
 emulator_uuid = ret.stdout.strip()
+
 
 try:
     with open(file_path, 'r') as file:
@@ -15,7 +16,7 @@ try:
             uuid=emulator_uuid,
         )
         print(format_config)
-        
+
 except Exception as e:
     print(f"An error occurred: {e}")
     exit(1)
