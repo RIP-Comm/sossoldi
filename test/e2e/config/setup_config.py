19edad97b42a43e66b7af36e8d5488393ee4f816
Simone Antonacci <simo.antonacci@gmail.com>
2025-03-27 19:10:10 +0000

    test

---
+++ test/e2e/config/setup_config.py
@@ -0,0 +1,27 @@
+import subprocess
+
+emulator_name = "android-emulator""
+file_path = "platform_config_template.yaml"
+command = f"adb -s {android_emulator} shell getprop emu.uuid"
+exception_raised = False
+
+ret = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
+emulator_uuid = ret.stdout.strip()
+
+try:
+    with open(file_path, 'r') as file:
+        content = file.read()
+        format_config = content.format(
+            uuid=emulator_uuid,
+        )
+        print(format_config)
+except FileNotFoundError:
+    print(f"The file at {file_path} was not found.")
+    exception_raised = True
+except Exception as e:
+    print(f"An error occurred: {e}")
+    exception_raised = True
+
+if exception_raised:
+    print("Can't setup the config file, aborting.")
+    exit(1)
