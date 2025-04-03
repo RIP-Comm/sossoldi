faac763c215f7f4827ace01f29b3ac0a4b668d0a
fres-sudo <francescocalicchio@hotmail.com>
2025-04-03 11:21:15 +0200

    ref(assets): moved flutter assets to images/ folder

--- lib/ui/assets.dart
+++ lib/ui/assets.dart
@@ -1,6 +1,6 @@
 /// Assets class to manage all the assets in the app
 ///
 abstract final class SossoldiAssets {
-  static String get openVault => "assets/images/logo.png";
+  static String get openVault => "assets/images/open_vault.png";
   static String get wallet => "assets/images/wallet.png";
 }
