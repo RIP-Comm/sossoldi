31e5b66f0dc378ac1e9ce4c08111e598a3cf6619
Dario Varriale <dario.varriale@atobit.it>
2025-04-06 16:41:38 +0200

    refactor: simplify ListTab widget by replacing Container with switch expression for better readability and error handling

--- lib/features/transactions_page/presentation/list_tab.dart
+++ lib/features/transactions_page/presentation/list_tab.dart
@@ -1,36 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../constants/functions.dart';
 import '../../../custom_widgets/transactions_list.dart';
 import '../../../providers/transactions_provider.dart';
 
-class ListTab extends ConsumerWidget with Functions {
+class ListTab extends ConsumerWidget {
   const ListTab({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final asyncTransactions = ref.watch(transactionsProvider);
 
-    return Container(
-      child: asyncTransactions.when(
-        data: (transactions) {
-          return TransactionsList(
-            margin: const EdgeInsets.all(16.0),
-            transactions: transactions,
-          );
-        },
-        loading: () {
-          return Container(
-            color: Theme.of(context).colorScheme.primaryContainer,
-          );
-        },
-        error: (error, stackTrace) {
-          return Center(
-            child: Text(stackTrace.toString()),
-          );
-        },
-      ),
-    );
+    return switch (asyncTransactions) {
+      AsyncData(:final value) => TransactionsList(
+          margin: const EdgeInsets.all(16.0),
+          transactions: value,
+        ),
+      AsyncError(:final error) => Center(
+          child: Text('Error: $error'),
+        ),
+      _ => Container(
+          color: Theme.of(context).colorScheme.primaryContainer,
+          child: const Center(
+            child: CircularProgressIndicator.adaptive(),
+          ),
+        ),
+    };
   }
 }
