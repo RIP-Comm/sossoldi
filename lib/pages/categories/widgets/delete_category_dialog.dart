15698282e9d4ec2d76658a4b592a752d04da3dc6
napitek <root@napitektv.com>
2025-04-19 15:50:09 +0200

    Delete category dialog review with ref.invalidates

--- lib/pages/categories/widgets/delete_category_dialog.dart
+++ lib/pages/categories/widgets/delete_category_dialog.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../model/category_transaction.dart';
+import '../../../providers/budgets_provider.dart';
 import '../../../providers/categories_provider.dart';
+import '../../../providers/dashboard_provider.dart';
+import '../../../providers/statistics_provider.dart';
+import '../../../providers/transactions_provider.dart';
 
 Future<void> showDeleteCategoryDialog(
     BuildContext context, WidgetRef ref, selectedCategory) async {
+  
   void backToCategoryList() {
     if (context.mounted) {
       Navigator.of(context)
           .popUntil((route) => route.settings.name == '/category-list');
     }
+  }
+
+  void invalidateProviders() {
+    ref.invalidate(categoriesProvider(userCategoriesFilter));
+    ref.invalidate(categoryByIdProvider);
+    ref.invalidate(transactionsProvider);
+    ref.invalidate(budgetsProvider);
+    ref.invalidate(dashboardProvider);
+    ref.invalidate(lastTransactionsProvider);
+    ref.invalidate(statisticsProvider);
   }
 
   return showDialog<void>(
@@ -38,12 +53,11 @@ Future<void> showDeleteCategoryDialog(
                 style: TextStyle(color: Theme.of(context).colorScheme.primary),
               ),
               onPressed: () async {
-                ref
+                await ref
                     .read(categoriesProvider(userCategoriesFilter).notifier)
-                    .markAsDeleted(selectedCategory.id)
-                    .whenComplete(backToCategoryList);
-                ref.invalidate(categoriesProvider(userCategoriesFilter));
-                ref.invalidate(categoryByIdProvider);
+                    .markAsDeleted(selectedCategory.id);
+                invalidateProviders();
+                backToCategoryList();
               }),
           TextButton(
               child: Text(
@@ -51,12 +65,11 @@ Future<void> showDeleteCategoryDialog(
                 style: TextStyle(color: Theme.of(context).colorScheme.primary),
               ),
               onPressed: () async {
-                ref
+                 await ref
                     .read(categoriesProvider(userCategoriesFilter).notifier)
-                    .removeCategory(selectedCategory.id!)
-                    .whenComplete(backToCategoryList);
-                ref.invalidate(categoriesProvider(userCategoriesFilter));
-                ref.invalidate(categoryByIdProvider);
+                    .removeCategory(selectedCategory.id!);
+                invalidateProviders();
+                backToCategoryList();
               }),
         ],
       );
