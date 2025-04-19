002b1788c0be1e4f873e3c716f634bf395913124
Dario Varriale <dario.varriale@atobit.it>
2025-04-19 10:38:47 +0200

    refactor: add CollaboratorsPage, MoreInfoPage, and PrivacyPolicyPage components with updated import paths for better organization

--- lib/features/more_info_page/presentation/more_info.dart
+++ lib/features/more_info_page/presentation/more_info.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../custom_widgets/default_card.dart';
+import '../../../custom_widgets/default_card.dart';
 
 class MoreInfoPage extends ConsumerStatefulWidget {
   const MoreInfoPage({super.key});
