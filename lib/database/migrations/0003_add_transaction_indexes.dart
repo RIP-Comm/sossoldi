42b614bc7b85436b296dea06c849f692678e6d20
xDefcon <luigi@xdefcon.com>
2025-05-09 00:33:34 +0200

    feat(database): Run dart format

--- lib/database/migrations/0003_add_transaction_indexes.dart
+++ lib/database/migrations/0003_add_transaction_indexes.dart
@@ -2,17 +2,19 @@ import 'package:sqflite/sqflite.dart';
 import '../migration_base.dart';
 
 class AddTransactionIndexes extends Migration {
-  AddTransactionIndexes() :
-        super(
-          version: 3,
-          description: 'Add transaction indexes for better query performance'
-      );
+  AddTransactionIndexes()
+      : super(
+            version: 3,
+            description:
+                'Add transaction indexes for better query performance');
 
   @override
   Future<void> up(Database db) async {
     // Create indexes for common query patterns
-    await db.execute('CREATE INDEX idx_transaction_date ON "transaction"(date)');
-    await db.execute('CREATE INDEX idx_transaction_type ON "transaction"(type)');
+    await db
+        .execute('CREATE INDEX idx_transaction_date ON "transaction"(date)');
+    await db
+        .execute('CREATE INDEX idx_transaction_type ON "transaction"(type)');
     await db.execute(
         'CREATE INDEX idx_transaction_id_bank_account ON "transaction"(idBankAccount)');
   }
