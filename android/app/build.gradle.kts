d057be4dde8a44ff92cd4aab51277ca8c9c8662c
Gabriele <gabrielecabriniii@gmail.com>
2025-08-02 13:54:27 +0200

    build: fix debug build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -53,10 +53,10 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
             storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
+            storePassword = keystoreProperties["storePassword"] as String?
         }
     }
 
