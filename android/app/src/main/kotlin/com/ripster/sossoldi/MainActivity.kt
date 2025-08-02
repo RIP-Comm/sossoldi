a170a3edea846cf2bbd5bf3c46ef8f4cf31e941d
Gabriele <gabrielecabriniii@gmail.com>
2025-08-02 14:06:17 +0200

    feat(auth): add local device authentication

--- android/app/src/main/kotlin/com/ripster/sossoldi/MainActivity.kt
+++ android/app/src/main/kotlin/com/ripster/sossoldi/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.ripster.sossoldi
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
