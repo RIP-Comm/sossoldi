8a5aa401a1a86c66441831a6c91284a9a930fc45
Marco Perugini <peruginimarco.mp@gmail.com>
2025-09-28 13:14:59 +0200

    Fix available liquidity calculation

--- lib/providers/statistics_provider.dart
+++ lib/providers/statistics_provider.dart
@@ -2,6 +2,7 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../model/transaction.dart';
+import 'accounts_provider.dart';
 
 final currentYearMontlyTransactionsProvider =
     StateProvider<List<FlSpot>>((ref) => const []);
@@ -10,11 +11,30 @@ final statisticsProvider = FutureProvider<void>((ref) async {
   final currentYearMontlyTransaction =
       await TransactionMethods().currentYearMontlyTransactions();
 
-  double runningTotal = 0;
-  ref.read(currentYearMontlyTransactionsProvider.notifier).state =
-      currentYearMontlyTransaction.map((e) {
-    runningTotal += e['income'] - e['expense'];
-    return FlSpot(double.parse(e['month'].substring(5)) - 1,
-        double.parse(runningTotal.toStringAsFixed(2)));
-  }).toList();
+  final accountsAsync = ref.read(accountsProvider);
+  final accounts = accountsAsync.value ?? [];
+  double currentBalance =
+      accounts.fold(0.0, (sum, account) => sum + (account.total ?? 0));
+
+  List<FlSpot> spots = [];
+  double runningBalance = currentBalance;
+
+  final reversedTransactions = currentYearMontlyTransaction.reversed.toList();
+
+  for (int i = 0; i < reversedTransactions.length; i++) {
+    final monthData = reversedTransactions[i];
+    final monthIndex = double.parse(monthData['month'].substring(5)) - 1;
+
+    spots.add(
+        FlSpot(monthIndex, double.parse(runningBalance.toStringAsFixed(2))));
+
+    if (i < reversedTransactions.length - 1) {
+      runningBalance =
+          runningBalance - monthData['income'] + monthData['expense'];
+    }
+  }
+
+  spots.sort((a, b) => a.x.compareTo(b.x));
+
+  ref.read(currentYearMontlyTransactionsProvider.notifier).state = spots;
 });
