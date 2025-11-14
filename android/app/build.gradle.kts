f02954ccc5081d205a300a74a83424a73d19711d
Mike V. <mike@cyway.it>
2025-11-14 14:26:35 +0100

    conditionally set signing config for release builds

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -64,7 +64,9 @@ android {
     buildTypes {
 
         release {
-            signingConfig = signingConfigs.getByName("release")
+            if (!gradle.startParameter.taskNames.any { it.toLowerCase().contains("fdroid") }) {
+                signingConfig = signingConfigs.getByName("release")
+            }
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
